afb7c5a87bdeaed9664ecf06c7c7df5171fc6cb6
mpalmr <matthew.palmer1@mohawkcollege.ca>
2024-08-22 10:39:46 -0400

    working google web results

---
+++ src/background.ts
@@ -0,0 +1,17 @@
+chrome.runtime.onMessage.addListener(({ action, data }, sender, sendRes) => {
+  if (action === 'sendGoogleResults') {
+    fetch('http://localhost:3000/results/google', {
+      method: 'POST',
+      headers: {
+        Accepts: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ...data,
+        userId: '1644d3b7-e7bc-4869-b9df-ea0e10317881',
+      }),
+    })
+      .then((res) => res.text())
+      .then(console.log);
+  }
+});
